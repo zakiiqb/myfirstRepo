2b2c592f71c0c7382137349260ed466ff4ae8ff5
Zaki <zaki.iqbal@tekschool.us>
2023-02-20 20:10:42 -0800

    Unit test class added

---
+++ src/Test/UnitTest.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class UnitTest {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 15;
+		int c = a + b;
+		
+		System.out.println(c);
+
+		
+
+	}
+
+}
