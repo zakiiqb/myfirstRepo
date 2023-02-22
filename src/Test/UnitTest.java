8c7775e6ae28d56a1959e50924fd620382beddd2
Zaki <zaki.iqbal@tekschool.us>
2023-02-21 16:43:06 -0800

    new method added and old one deleted

--- src/Test/UnitTest.java
+++ src/Test/UnitTest.java
@@ -4,14 +4,15 @@ public class UnitTest {
 
 	public static void main(String[] args) {
 
-		int a = 10;
-		int b = 15;
-		int c = a + b;
-		
-		System.out.println(c);
 
-		
+		System.out.println(testone(10,5));
 
+	}
+	
+	public static int testone(int a, int b) {
+		
+		return a-b;
+		
 	}
 
 }
